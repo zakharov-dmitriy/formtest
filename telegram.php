9f73c1c3b0a26346c148678cbd2ccc7309bf4cb9
zakharov-dmitriy <65556295+zakharov-dmitriy@users.noreply.github.com>
2020-09-28 11:07:19 +0700

    Add files via upload

---
+++ telegram.php
@@ -0,0 +1,32 @@
+<?php
+
+/* https://api.telegram.org/botXXXXXXXXXXXXXXXXXXXXX/getUpdates,
+где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
+
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$email = $_POST['email'];
+$msg = $_POST['msg'];
+$token = "988482545:AAHDb6iPR4PNMDq_AVDb6dvkP06r3QyD5uU";
+$chat_id = "-448086899";
+$arr = array(
+  'Имя пользователя: ' => $name,
+  'Телефон: ' => $phone,
+  'Email' => $email,
+  'Сообщение:' => $msg
+);
+
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+$sendToTelegram2 = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+if ($sendToTelegram && $sendToTelegram2) {
+  header('Location: Thanks.html');
+} else {
+  echo "Error";
+}
+?>
